6c8392c5ef1d90d1a2f137a16c8b6ef22b38b95f
Ayoub Ridouani <ayoub.ridouani@usmba.ac.ma>
2024-02-14 14:20:00 +0100

    combine reducers

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const redux = require('redux');
 
 const createStore = redux.createStore;
+const combineReducers = redux.combineReducers;
 
 // State
-const initialState = {
+const initialStateOfCake = {
     total_of_existed_cakes: 100
 }
 
-// Actions
+const initialStateOfIceCream = {
+    total_of_existed_iceCreams: 100
+}
+
+// Actions of Cake
 const buyCake = (sum_of_taked_cakes) => {
     return {
         type: 'BUY_CAKE',
@@ -17,8 +22,18 @@ const buyCake = (sum_of_taked_cakes) => {
     };
 }
 
-// Reducer
-const reducer = (state = initialState, action) => {
+// Actions of Ice Cream
+const buyIceCream = (sum_of_taked_ice_creams) => {
+    return {
+        type: 'BUY_ICECREAM',
+        payload: {
+            sum_of_taked_ice_creams: sum_of_taked_ice_creams
+        }
+    };
+}
+
+// Reducer of Cake
+const reducerCake = (state = initialStateOfCake, action) => {
     switch(action.type){
         case 'BUY_CAKE':
             return {
@@ -29,9 +44,24 @@ const reducer = (state = initialState, action) => {
     }
 }
 
+// Reducer of Ice Cream
+const reducerIceCream = (state = initialStateOfIceCream, action) => {
+    switch(action.type){
+        case 'BUY_ICECREAM':
+            return {
+                total_of_existed_iceCreams: state.total_of_existed_iceCreams - action.payload.sum_of_taked_ice_creams
+            }
+        default:
+            return state;
+    }
+}
+
 // Store
-const store = createStore(reducer);
-console.log('Initial State: ', initialState);
+rootReducers = combineReducers({
+    cake: reducerCake,
+    iceCream: reducerIceCream
+})
+const store = createStore(rootReducers);
 
 // subscribe(listener)​: Adds a change listener. 
 // It will be called any time an action is dispatched, and some part of the state tree may potentially have changed. 
@@ -42,9 +72,10 @@ store.dispatch(buyCake(1));
 store.dispatch(buyCake(1));
 store.dispatch(buyCake(3));
 store.dispatch(buyCake(1));
-console.log('Current State: ', store.getState());
+
+store.dispatch(buyIceCream(1));
+store.dispatch(buyIceCream(1));
+store.dispatch(buyIceCream(3));
+store.dispatch(buyIceCream(1));
 
 unsubscribe();
-
-store.dispatch(buyCake(1));
-console.log('Current State: ', store.getState());
